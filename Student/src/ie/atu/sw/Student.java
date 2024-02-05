8bcd9755626dc510806208ca6889fd4867990dad
Andrii_Desktop <andrii.skvortsov.ua@gmail.com>
2024-02-05 18:51:59 +0000

    updated hellowrold with a response

---
+++ Student/src/ie/atu/sw/Student.java
@@ -0,0 +1,18 @@
+package ie.atu.sw;
+
+public class Student {
+
+    /*
+     * This is how we initialise Student in Java.
+     */
+
+    public Student() {
+        System.out.println(this);
+    }
+
+    public static void main(String[] args) {
+        new Student(); // Create a new Student object
+
+    }
+
+}
