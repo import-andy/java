f8c028c56e651721ca9feb5c00a32b33d3db7aec
Andrii_Desktop <andrii.skvortsov.ua@gmail.com>
2024-02-05 19:50:00 +0000

    Added comments

--- helloworld/src/ie/atu/sw/Hello.java
+++ helloworld/src/ie/atu/sw/Hello.java
@@ -2,6 +2,8 @@ package ie.atu.sw;
 
 public class Hello {
 	
+	// generates some simple output
+
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 		System.out.println("How are you?");
