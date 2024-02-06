b044fcce4d7e289dd66ccdea9e975f7187d45d68
Andrii_Desktop <andrii.skvortsov.ua@gmail.com>
2024-02-06 23:47:27 +0000

    introducing variables

---
+++ vars_and_assignment_statements/vars_and_assignment_statements.java
@@ -0,0 +1,26 @@
+
+// Creating a variables through an assign statement & printing them from newling (println)
+
+class VarsAndAssigmnentStatemens {
+    public static void main(String[] args) {
+
+        // Setting variables
+        String hello;
+        hello = "Hello, world!";
+        int day;
+        day = 6;
+        int month;
+        month = 2;
+        int year;
+        year = 2024;
+
+        // Printing some stuff with variables
+        System.out.println(hello);
+        System.out.print("This program was written on ");
+        System.out.print(day);
+        System.out.print("/");
+        System.out.print(month);
+        System.out.print("/");
+        System.out.print(year);
+    }
+}
