8164df0eb3343faba1b774e8bffb35421189cea7
Andrii_Desktop <andrii.skvortsov.ua@gmail.com>
2024-02-08 00:25:53 +0000

    Added javaBasics file

---
+++ javaBasics/javaBasics.java
@@ -0,0 +1,45 @@
+/*
+JAVA BASICS:
+ 
+TYPES OF DATA:
+    Primitive ones:
+        boolean - true, false
+        byte - 16 bits (binary digits 0 and 1)
+        short - 16 bits: [2^-15; 2^15]
+        int - 32 bits: [2^-31; 2^31]
+        long - 64 bits: [2^-63; 2^63]
+        float - 1 + 8 + 23 bit floating-point number: [2^-149; (2-2^-23)2^127]
+        double - 1 + 11 + 52 bit floating-point number:  [2^1074; (2-2^-52)2^1023]
+        char - Unicode character, 16 bits
+    These are numeric convertible types.
+    Represented by a sequence of bits (0 and 1).
+    Default value is zero.
+
+    More complex ones:
+        java.lang.String - string
+        java.util.Date - date
+
+QUOTES:
+    Single quotes for single characters: 'G'
+    Double quotes for more that 1 character: "GG"
+
+NAMES:
+    Camel case
+    For classes start with a capital letter: class ArtNuove
+    For everything elese like var start with a small letter: String mySurname
+
+VARIABLES
+    1. Instance variables :
+        Are variables declared at a class level.
+        Should always be private.
+        Are given default values:
+            boolean: false
+            numeric: 0
+            objects: null
+    2. Local Variables:
+        Local variables have local scope.
+        They can only be seen inside the the curly brackets within which
+            they were declared. Local variables are not given default values.
+
+
+ */
