64f7ce8aa188fca8a0c7d954b0d901a9beee30c2
Andrii_Desktop <andrii.skvortsov.ua@gmail.com>
2024-02-18 13:50:55 +0000

    updated ClasesAndObjects

--- ClasesAndObjects/ie/atu/sw/Runner.java
+++ ClasesAndObjects/ie/atu/sw/Runner.java
@@ -1,8 +1,28 @@
 package ie.atu.sw;
 
+import java.time.LocalDate;
+
 public class Runner {
     public static void main(String[] args) {
-        Student s = new Student();
         
+        // creating new instance of a class (using "new" keyword)
+        Student s = new Student(); 
+
+        System.out.println("Student Object ID from Runner: " + s);
+
+        s.sid = 62158454;
+        s.name = "Andrii Skvortsov";
+        s.dob = LocalDate.now();
+
+        // invoking methods
+        s.register();
+        s.study();
+        s.execute();
+
+        // accessing the state
+        System.out.println(s.sid);
+        System.out.println(s.name);
+        System.out.println(s.dob);
+        System.out.println(s.registered);
     }
 }
--- ClasesAndObjects/ie/atu/sw/Student.java
+++ ClasesAndObjects/ie/atu/sw/Student.java
@@ -1,5 +1,34 @@
 package ie.atu.sw;
 
+import java.time.LocalDate;
+
 public class Student {
 
+    /*
+     * Defining some state (instance variables) for the class Student.
+     * They will define what the instances of a class will knwow.
+     * (the best practice is to make instance inst. vars below PRIVATE.
+     * We make them public only for the training purpose)
+     */
+    public long sid;
+    public String name = "Undefined";
+    public LocalDate dob;
+    public boolean registered = false;
+
+    public Student() {
+        System.out.println("The ID of this Student object is " + this);
+    }
+
+    // creating methods of a class
+    public void register() {
+        this.registered = true;
+    }
+
+    public void study() {
+        System.out.println("Student " + name + " is studying.");
+    }
+
+    public void execute() {  // bad name for a method
+        System.out.println("Executed.");
+    }
 }
